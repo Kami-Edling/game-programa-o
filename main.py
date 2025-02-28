36e39536b862c1846b96040f4976152e004ac5e7
Kamilla Edling <moreirakamilla@hotmail.com>
2025-02-28 09:26:53 -0300

    Creating main loop Connecting Menu and Game classes

--- main.py
+++ main.py
@@ -1,33 +1,5 @@
-# This is a sample Python script.
+from codeGame.game  import Game
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-import pygame
-
-print('Setup Start')
-pygame.init()
-window = pygame.display.set_mode(size = (600, 480))
-print('Setup End')
-
-print('Loop Start')
-while True:
-  # Check for all events
-   for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() #Clocse Window
-            quit() # end pygame
+game = Game()
+game.run()
 
